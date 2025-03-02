fb65e538a0e39e877ab03b21048af247a2c10803
vempr <alexnguyena6@gmail.com>
2025-03-02 20:59:22 +0100

    db reference employer to user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 
 use App\Models\Employer;
 use App\Models\Job;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder {
@@ -13,7 +14,8 @@ class DatabaseSeeder extends Seeder {
      * Seed the application's database.
      */
     public function run(): void {
-        $employers = Employer::factory(5)->create();
+        $users = User::factory(5)->create();
+        $employers = Employer::factory(5)->recycle($users)->create();
         Job::factory(30)->recycle($employers)->create();
     }
 }
